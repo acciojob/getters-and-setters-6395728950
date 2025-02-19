92a3263d3f58ac25ea78b3e6b4541d09db1c5b0e
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-19 15:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 //complete this code
 class Person {
-	public:
-	string name,
-	let age,
-	Person(Name,Age){
-		this.name = Name,
-	      this.age =Age,
-	}
- string get name(){
-	  return name;
-  }
- set age(age){
-	 age = age;
- }
-};
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    get name() {
+        return this.name;
+    }
+
+    set age(age) {
+        this.age = age;
+    }
+}
 
 class Student extends Person {
-	
-	void study(){
-		console.log(`${name} is studying`);
-	}
+    study() {
+        console.log(this.name + " is studying");
+    }
 }
 
 class Teacher extends Person {
-	void teach(){
-		console.log(`${name} is teaching`);
-	}
+    teach() {
+        console.log(this.name + " is teaching");
+    }
 }
 
 // Do not change the code below this line
