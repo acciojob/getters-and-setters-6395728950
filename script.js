e645f8b7586ae11b2b5cb4651d7babf8b7c72862
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-19 15:18:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	public:
+	string name,
+	let age,
+	Person(Name,Age){
+		this.name = Name,
+	      this.age =Age,
+	}
+ string get name(){
+	  return name;
+  }
+ set age(age){
+	 age = age;
+ }
+};
 
-class Student extends Person {}
+class Student extends Person {
+	
+	void study(){
+		console.log(`${name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	void teach(){
+		console.log(`${name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
